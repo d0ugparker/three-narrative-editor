96ca2ed50e822505e293a692cb79f904e93ccfbc
Doug Parker <doug@dougs-mini.local>
2026-06-07 18:14:54 -0700

    Initial commit with Emacs and Markdown files

--- tne-render.el
+++ tne-render.el
@@ -1,9 +1,34 @@
 (require 'tne-model)
+
 (defun tne-render-segments (segments)
- (let ((line ""))
-  (dolist (s (sort (copy-sequence segments)
-   (lambda (a b) (< (tne-segment-start-column a) (tne-segment-start-column b)))))
-   (while (< (length line) (tne-segment-start-column s)) (setq line (concat line " ")))
-   (setq line (concat line (tne-segment-text s))))
-  line))
+(let* ((sorted
+(sort (copy-sequence segments)
+(lambda (a b)
+(< (tne-segment-start-column a)
+(tne-segment-start-column b)))))
+(result "")
+(current-column 1))
+
+(dolist (s sorted)
+
+  (let* ((start (tne-segment-start-column s))
+         (text  (tne-segment-text s))
+         (padding (max 0 (- start current-column))))
+
+    (setq result
+          (concat result
+                  (make-string padding ?\s)
+                  text))
+
+    (setq current-column
+          (+ start (length text))))
+
+  (unless (eq s (car (last sorted)))
+    (setq result
+          (concat result " | "))
+    (setq current-column
+          (+ current-column 3))))
+
+result))
+
 (provide 'tne-render)
