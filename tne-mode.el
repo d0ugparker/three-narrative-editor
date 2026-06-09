51c14e9fe048db636ddb0045d721e42a3383cf78
Doug Parker <ispinn@gmail.com>
2026-06-08 17:40:19 -0700

    Pre V0.2.5 wrapped rendering

--- tne-mode.el
+++ tne-mode.el
@@ -57,59 +57,6 @@
 
     (nreverse result)))
 
-(defun tne-wrap-text (text width)
-  (let ((words (split-string text " "))
-        (lines nil)
-        (current "")
-        (forced-splits 0))
-
-    (while words
-      (let ((word (car words)))
-
-        (cond
-
-         ;; long word on empty line
-         ((and (string= current "")
-               (> (length word) width))
-
-          (push (substring word 0 width) lines)
-
-          (setq words
-                (cons (substring word width)
-                      (cdr words)))
-
-          (setq forced-splits
-                (1+ forced-splits)))
-
-         ;; word fits
-         ((<= (+ (length current)
-                 (if (string= current "") 0 1)
-                 (length word))
-              width)
-
-          (setq current
-                (if (string= current "")
-                    word
-                  (concat current " " word)))
-
-          (setq words (cdr words)))
-
-         ;; wrap
-         (t
-
-          (push current lines)
-          (setq current "")))))
-
-    (unless (string= current "")
-      (push current lines))
-
-    (setq lines (nreverse lines))
-
-    (list
-     :lines lines
-     :height (length lines)
-     :forced-splits forced-splits)))
-
 (defun tne-layout-report ()
   (interactive)
 
